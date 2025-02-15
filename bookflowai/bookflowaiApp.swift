c6dceb3eaabb3c6a107e0e81be9d9e268f4d0aa1
Mykhailo Platonov <mihailplatonov@mac-mini-mihail-2.home>
2025-02-15 16:02:53 +0100

    init commit

---
+++ bookflowai/bookflowaiApp.swift
@@ -0,0 +1,14 @@
+//
+//  bookflowaiApp.swift
+//  bookflowai
+
+import SwiftUI
+
+@main
+struct bookflowaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bookflowaiTests/bookflowaiTests.swift
@@ -0,0 +1,14 @@
+//
+//  bookflowaiTests.swift
+//  bookflowaiTests
+
+import Testing
+@testable import bookflowai
+
+struct bookflowaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
